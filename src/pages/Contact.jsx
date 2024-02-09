fcce196b5f5c74230c4281e08c4842b42b6e1663
rambriz91 <rambriz91@gmail.com>
2024-02-08 18:52:13 -0800

    added embedded map to contact us and contact information.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,24 @@
 export default function Contact() {
   return (
-    <div className='flex flex-col text-2xl georgia'>
-      <h2 className='flex text-center m-5 georgia'>ተገናኝ / Contact</h2>
-      <div className='flex flex-col'>
-        <div></div>
-        <div></div>
-      </div>
+    <div className='w-full'>
+      <h2 className='text-center m-5 georgia w-full'>ተገናኝ / Contact</h2>
+      <div className='flex w-full justify-around'>
+        <div className='m-5 flex flex-col georgia text-2xl w-1/2 items-center'>
+          <p>Email: SaintAbuneAregawiOrthodox@gmail.com</p>
+          <p>Phone: (619) 567-9416</p>
+          <p>Address: 5202 Orange Ave, San Diego, CA 92115</p>
+        </div>
+        <div className='m-5 flex justify-start w-1/2'>
+          <iframe
+            src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3355.446921646841!2d-117.0856721229388!3d32.75387387367327!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d95421d81d5441%3A0x6c729a0aa446ee35!2s5202%20Orange%20Ave%2C%20San%20Diego%2C%20CA%2092115!5e0!3m2!1sen!2sus!4v1707444184902!5m2!1sen!2sus'
+            width='600'
+            height='450'
+            allowfullscreen=''
+            loading='lazy'
+            referrerpolicy='no-referrer-when-downgrade'
+          ></iframe>
+        </div>
+        </div>
     </div>
   );
 }
