7806516dd7a876631b2ff8ca7d373ae704c72026
rambriz91 <rambriz91@gmail.com>
2024-02-20 16:37:23 -0800

    corrected spelling error from tigre to tigray on the about us section.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
       <img className= "flex w-10/12 m-5" src="/images/St.Aregawi Banner.png" alt="Church Banner" />
       <p className='w-10/12 m-5'>
         እርስዎ እንዲቀላቀሉን ተጋብዘዋል! እኛ ትንሽ፣ ተለዋዋጭ፣ ወግ አጥባቂ ጉባኤ ነን፣ በሳን ዲዬጎ፣ CA። ቤተሰብ፣
-        ባህልና ጉባኤ የማንነታችን ሦስት ምሰሶዎች ናቸው። ብዙ ምእመናኖቻችን አማርኛ እና ትግሬን አቀላጥፈው ስለሚናገሩ
+        ባህልና ጉባኤ የማንነታችን ሦስት ምሰሶዎች ናቸው። ብዙ ምእመናኖቻችን አማርኛ እና ትግራይ አቀላጥፈው ስለሚናገሩ
         እንግሊዝኛ እየተማሩ ከአካባቢያችን ጎረቤቶች ጋር ለመግባባት እና ለማክበር። ወደ እኛ አስደናቂ የሳንዲያጎ
         ማህበረሰብ ለማዋሃድ እንድንችል በተለይ ስደተኞችን እንቀበላለን። እባክዎ በማንኛውም ሳምንታዊ አገልግሎታችን
         ይቀላቀሉን። ኢሜል ላኩልን እና እርስዎን ለመቀበል እና ከአዲሱ የቤተክርስቲያን ቤተሰብ ጋር ለማስተዋወቅ አምባሳደር
@@ -16,7 +16,7 @@ export default function About() {
         YOU are invited to join us! We are a small, dynamic, conservative
         congregation, centrally located in San Diego, CA. Family, culture and
         congregation are three pillars of who we are. Many of our congregants
-        are fluent in Amharic and Tigre, learning English to communicate with
+        are fluent in Amharic and Tigray, learning English to communicate with
         and honor our local neighbors. We particularly welcome immigrants so we
         may help integrate them into our wonderful San Diego Community. Please
         join us at any of our weekly services. Send us an email and we will
