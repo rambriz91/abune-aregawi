badbf28371ed37fd35a10f8cba2f33b44bf22b4f
rambriz91 <rambriz91@gmail.com>
2024-02-27 08:50:48 -0800

    added events page.

---
+++ src/pages/Event.jsx
@@ -0,0 +1,7 @@
+export default function Event() {
+    return (
+        <div className='flex flex-col text-2xl georgia items-center'>
+            <h2 className='text-center m-5'>Events</h2>
+        </div>
+    )
+}
