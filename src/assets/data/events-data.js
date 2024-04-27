0a61e88884d84a935dee33450c764a5cdb31cf89
rambriz91 <rambriz91@gmail.com>
2024-04-27 09:02:59 -0700

    added events data and uploaded images for them.

---
+++ src/assets/data/events-data.js
@@ -0,0 +1,82 @@
+const eventsData = [
+    {
+        id: 1,
+        eventName:'Saint Aregawi',
+        img:'/images/aregawi-mural.jpg',
+        alt: 'Mural of St. Aregawi',
+    },
+    {
+        id: 2,
+        eventName: "Holy Trinity",
+        img: '/images/Three wise men.jpg',
+        alt:'Mural of Holy Trinity'
+    },
+    {
+        id: 3,
+        eventName: 'Virgin Mary',
+        img: '/images/Virgin-mary-christ.jpg',
+        alt: 'Mural of Virgin Mary'
+    },
+    {
+        id: 4,
+        eventName: 'Fellowship',
+        img: '/images/fellowship.png',
+        alt: 'Fellowship Mural'
+    },
+    {
+        id: 5,
+        eventName: 'Baptism of Our Babies',
+        img:'/images/baptism.jpg',
+        alt: 'Mural Depicting babtism'
+    },
+    {
+        id: 6,
+        eventName:'Traditional Celebrations',
+        img:'/images/Christ-the-servant.jpg',
+        alt:'Mural of Jesus washing Disciples Feet'
+    },
+    {
+        id: 7,
+        eventName:'Feeding the Homeless and the Faithful',
+        img:'/images/last-supper-angels-munir-alawi.jpg',
+        alt: 'Mural of Final Supper'
+    },
+    {
+        id: 8,
+        eventName:'Religious Education',
+        img:'/images/Illumination-from-Abba-Garima-gospel.jpg',
+        alt:'Mural of pious saint'
+    },
+    {
+        id:  9,
+        eventName: 'Caring for our Elderly',
+        img:'/images/elderly.png',
+        alt: 'Elderly Man'
+    },
+    {
+        id: 10,
+        eventName:'Assisting Refugees',
+        img:'/images/ark-of-the-covenant.jpg',
+        alt:'Mural of two saints'
+    },
+    {
+        id:11,
+        eventName: 'Annual Ethipian Breakfast Fundraiser',
+        img:'/images/fundraiser.jpg',
+        alt:'Mural of the 12 disciples of Christ'
+    },
+    {
+        id:12,
+        eventName: 'Wedding',
+        img:'/images/wedding.png',
+        alt:'Mural of Virgin Mary providing her blessing'
+    },
+    // {
+    //     id:
+    //     eventName:
+    //     img:
+    //     alt:
+    // }
+];
+
+export default eventsData;
