a1bad978ff33eba51f520f63847b0a48fc2bd943
rambriz91 <rambriz91@gmail.com>
2024-04-27 09:34:32 -0700

    events page now displays event divs.

--- src/components/EventsDiv.jsx
+++ src/components/EventsDiv.jsx
@@ -2,10 +2,10 @@ export default function Activity(props) {
 const events = props.eventsData;
 return (
 <>
-<section>
+<section className='flex flex-wrap w-full'>
     {events.map((evnt) => (
-        <div className='flex flex-col p-4 items-center w-1/4' key={evnt.id}>
-            <img src={evnt.img} alt={evnt.alt} />
+        <div className='flex flex-col p-1 items-center w-1/4' key={evnt.id}>
+            <img className='thumbnail' src={evnt.img} alt={evnt.alt} />
             <h4>{evnt.eventName}</h4>
         </div>
     ))}
--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,11 +1,11 @@
 import Activity from '../components/EventsDiv';
-import data from '../assets/data/events-data';
+import eventsData from '../assets/data/events-data';
 
 export default function Event() {
   return (
     <div className='flex flex-col text-2xl georgia items-center'>
       <h2 className='text-center m-5'>ክስተት / Events</h2>
-      <Activity data ={data} />
+      <Activity eventsData ={eventsData} />
     </div>
   );
 }
